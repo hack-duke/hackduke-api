e1eb74d39b54329bec63d528b64990520eceebc3
Andy <ownz.andy@gmail.com>
2016-08-27 22:21:35 -0400

    fixing push env bug

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,6 +1,6 @@
 require 'pusher'
 
-Pusher.app_id = ENV['PUSHER_']
+Pusher.app_id = ENV['PUSHER_API_ID']
 Pusher.key = ENV['PUSHER_KEY']
 Pusher.secret = ENV['PUSHER_SECRET']
 Pusher.logger = Rails.logger
