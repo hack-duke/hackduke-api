f04b9a9961b840716d02288b491ddec43429109e
Andy <ownz.andy@gmail.com>
2016-09-17 22:31:18 -0400

    fix null fields on typeform webhook

--- app/modules/typeform_webhook.rb
+++ app/modules/typeform_webhook.rb
@@ -30,13 +30,15 @@ module TypeformWebhook
 
   def create_webhook_info_hash(model, fields, answers)
     hash = {}
-    model.column_names.each do |model_field|
-      result = extract_webhook_result(fields, model_field, answers) 
-      if valid_result(result, model_field)
-        if model.new[model_field] == nil
-          result = result[0]
+    if fields != null
+      model.column_names.each do |model_field|
+        result = extract_webhook_result(fields, model_field, answers) 
+        if valid_result(result, model_field)
+          if model.new[model_field] == nil
+            result = result[0]
+          end
+          hash[model_field.to_sym] = result
         end
-        hash[model_field.to_sym] = result
       end
     end
     hash
